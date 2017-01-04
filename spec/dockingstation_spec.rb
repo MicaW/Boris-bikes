cf85f9575e9156d254a2f26c3344a25726dcabf3
micawhitby <micawhitby@gmail.com>
2017-01-04 17:33:34 +0000

    Defined bike class and working? method, created test for this

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it {expect(DockingStation.new).to respond_to(:release_bike)}
+  it {expect(subject).to respond_to(:release_bike)}
 
 
 end
